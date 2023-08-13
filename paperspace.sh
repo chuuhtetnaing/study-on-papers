65b5f225e2eebee6457ca872aa46cd8512c547b5
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2023-08-13 06:38:13 +0000

    setup paperspace workplace

---
+++ paperspace.sh
@@ -0,0 +1,14 @@
+# Setup for Github
+cp -r ./ssh-keys ~/.ssh
+git config --global user.email "chuuhtetnaing@gmail.com"
+git config --global user.name "Chuu Htet Naing"
+
+# Update Repo and Install Required Libraries
+apt update && apt install aria2 -y
+
+# Uninstall Outdated Python Libraries
+pip3 uninstall torch torchvision torchaudio transformers -y
+
+# Install Required Python Libraries
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install transformers
